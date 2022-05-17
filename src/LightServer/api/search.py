565f8af50c2459155cd260fa51d577b095e3835c
N_XY <n.kubliy@gmail.com>
2022-05-17 18:51:39 +0300

    Search rules changed to options with lowercase latters to search for users by login

--- src/LightServer/api/search.py
+++ src/LightServer/api/search.py
@@ -10,10 +10,9 @@ router = APIRouter(
 )
 
 
-@router.post("/by_login", response_model=models.User)
+@router.post("/by_login", response_model=models.User, dependencies=[Depends(get_current_user)])
 async def get_user_by_login(
         login: str = Body(..., example='some_user_login'),
-        user: models.User = Depends(get_current_user),
         search_service: SearchService = Depends()
 ):
     return search_service.by_login(login)
--- src/LightServer/services/search.py
+++ src/LightServer/services/search.py
@@ -1,8 +1,9 @@
 from fastapi import Depends
 
-from .. import models
 from ..database import orm
 from ..database.database import Session, get_session
+
+from sqlalchemy import func
 
 
 class SearchService:
@@ -10,11 +11,11 @@ class SearchService:
     def __init__(self, session: Session = Depends(get_session)):
         self._session = session
 
-    def by_login(self, login: str) -> models.User:
+    def by_login(self, login: str) -> orm.User:
         another_user = (
             self._session
                 .query(orm.User)
-                .filter(orm.User.login == login)
+                .filter(orm.User.login == func.lower(login))
                 .first()
         )
         return another_user
