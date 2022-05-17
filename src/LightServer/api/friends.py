298eb83cedf85b45405d5e7de6170bf15184fbd3
N_XY <n.kubliy@gmail.com>
2022-05-17 18:41:17 +0300

    Code refactoring to use dependency instead of repeating code blocks

--- src/LightServer/api/friends.py
+++ src/LightServer/api/friends.py
@@ -2,7 +2,9 @@ from typing import List
 
 from fastapi import APIRouter, Header, Depends, Response, status, Body
 from .. import models
+from ..database import orm
 from ..services.auth import get_current_user
+from ..services.dependencies import get_user_from_id
 from ..services.friends import FriendsService
 
 router = APIRouter(
@@ -21,17 +23,17 @@ async def get_friends(
 
 @router.post("/add", status_code=status.HTTP_202_ACCEPTED)
 async def add_friend(
-        another_user_id: int,
+        another_user: orm.User = Depends(get_user_from_id),
         user: models.User = Depends(get_current_user),
         friends_service: FriendsService = Depends()
 ):
-    friends_service.add_friend(user, another_user_id)
+    friends_service.add_friend(user, another_user)
     return Response(status_code=status.HTTP_202_ACCEPTED)
 
 
 @router.delete("/del", status_code=status.HTTP_204_NO_CONTENT)
 async def remove_friend(
-        another_user_id: int = Body(...),
+        another_user_id: orm.User = Depends(get_user_from_id),
         user: models.User = Depends(get_current_user),
         friends_service: FriendsService = Depends()
 ):
--- src/LightServer/services/friends.py
+++ src/LightServer/services/friends.py
@@ -3,6 +3,7 @@ from typing import List
 
 from fastapi import Depends, exceptions, status, Response
 
+from .dependencies import get_user_from_id
 from ..database import orm
 from ..database.database import get_session, Session
 from .. import models
@@ -11,86 +12,59 @@ from .. import models
 class FriendsService:
 
     def __init__(self, session: Session = Depends(get_session)):
-        self.session = session
+        self._session = session
 
     def get_list(self, user: models.User) -> List[orm.User]:
-        friends_ids = json.loads((
-            self.session
-            .query(orm.User, orm.User.friend_list)
-            .filter(orm.User.id == user.id)
-            .first()
-        ).friend_list)
+        friends_ids = json.loads(get_user_from_id(user.id, session=self._session).friend_list)
 
         friends_list = (
-            self.session
+            self._session
             .query(orm.User)
             .where(orm.User.id.in_(friends_ids))
             .all()
         )
         return friends_list
 
-    def add_friend(self, user: models.User, another_user_id: int):
-        user = (
-             self.session
-             .query(orm.User)
-             .filter(orm.User.id == user.id)
-             .first()
-        )
-        another_user = (
-            self.session
-                .query(orm.User)
-                .filter(orm.User.id == another_user_id)
-                .first()
-        )
-
-        if not another_user:
-            raise exceptions.HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-
+    def add_friend(self, user: models.User, another_user: orm.User):
+        user = get_user_from_id(user.id, session=self._session)
         friends_ids: List = json.loads(user.friend_list)
 
-        if another_user_id in friends_ids:
+        if another_user.id in friends_ids:
             raise exceptions.HTTPException(status_code=status.HTTP_409_CONFLICT)
 
-        friends_ids.append(another_user_id)
+        friends_ids.append(another_user.id)
         user.friend_list = json.dumps(friends_ids)
-        self.session.commit()
+
+        self._session.commit()
 
         another_user_friends_ids: List = json.loads(another_user.friend_list)
+
         if user.id in another_user_friends_ids:
             raise exceptions.HTTPException(status_code=status.HTTP_409_CONFLICT)
 
         another_user_friends_ids.append(user.id)
         another_user.friend_list = json.dumps(another_user_friends_ids)
-        self.session.commit()
+        self._session.commit()
 
-    def remove_friend(self, user: models.User, another_user_id: int):
-        user = (
-            self.session
-                .query(orm.User)
-                .filter(orm.User.id == user.id)
-                .first()
-        )
+    def remove_friend(self, user: models.User, another_user: orm.User):
+        user = get_user_from_id(user.id, session=self._session)
+
         friends_ids: List = json.loads(user.friend_list)
 
-        if another_user_id not in friends_ids:
+        if another_user.id not in friends_ids:
             raise exceptions.HTTPException(status_code=status.HTTP_409_CONFLICT)
 
-        friends_ids.remove(another_user_id)
+        friends_ids.remove(another_user.id)
 
         user.friend_list = json.dumps(friends_ids)
-        self.session.commit()
+        self._session.commit()
 
-        another_user = (
-            self.session
-                .query(orm.User)
-                .filter(orm.User.id == another_user_id)
-                .first()
-        )
         another_user_friends_ids: List = json.loads(another_user.friend_list)
+
         if user.id not in another_user_friends_ids:
             raise exceptions.HTTPException(status_code=status.HTTP_409_CONFLICT)
 
         another_user_friends_ids.remove(user.id)
         another_user.friend_list = json.dumps(another_user_friends_ids)
-        self.session.commit()
+        self._session.commit()
 
